920f128a412872299955f6604c2af89052bb980a
thille <thille@thetus.com>
2013-07-03 10:32:16 -0700

    Added missing event handlers

--- src/public/app/controller/search/SearchBody.js
+++ src/public/app/controller/search/SearchBody.js
@@ -13,5 +13,34 @@ Ext.define('Savanna.controller.search.SearchBody', {
 
     views: [
         'Savanna.view.search.SearchBody'
-    ]
+    ],
+
+    currentPanel: 'searchoptions',
+
+    init: function (app) {
+        var me = this;
+        /*
+         These listeners toggle visibility between search options and search results
+         */
+        me.control({
+            'mainsearchbody > #searchbodytoolbar #optionsbutton': {
+                click: function (button, event) {
+                    if (me.currentPanel != "searchoptions") {
+                        button.up("mainsearchbody").queryById("mainsearchoptions").show();
+                        button.up("mainsearchbody").queryById("mainresults").hide();
+                        me.currentPanel = "searchoptions";
+                    }
+                }
+            },
+            'mainsearchbody > #searchbodytoolbar #resultsbutton': {
+                click: function (button, event) {
+                    if (me.currentPanel != "results") {
+                        button.up("mainsearchbody").queryById("mainsearchoptions").hide();
+                        button.up("mainsearchbody").queryById("mainresults").show();
+                        me.currentPanel = "results";
+                    }
+                }
+            }
+        });
+    }
 });
