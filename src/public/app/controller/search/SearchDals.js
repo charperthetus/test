ac4704df8988b6f1c631025b11d4de37cb16c13e
swatson <rufinitsw@hotmail.com>
2013-07-11 09:10:09 -0700

    'show search options' button now only displays if there are items in the customSearchGroup

--- src/public/app/controller/search/SearchDals.js
+++ src/public/app/controller/search/SearchDals.js
@@ -16,12 +16,12 @@ Ext.define('Savanna.controller.search.SearchDals', {
     layout: 'hbox',
 
     createPanel: function(myRecord) {
-        console.log(myRecord);
+        console.log('myRecord.customSearchGroups',myRecord.customSearchGroups().data.length);
         return Ext.create('Savanna.view.search.searchDals.SearchOptions', {
             itemId: myRecord.data.id,
             checkboxLabel: myRecord.data.displayName,
             label: myRecord.data.textDescription,
-            showButton: myRecord.data.customSearchGroups
+            showButton: myRecord.customSearchGroups().data.length
         });
     },
 
@@ -640,64 +640,7 @@ Ext.define('Savanna.controller.search.SearchDals', {
             "searchGeoTypes" : null,
             "supportsHyperDynamicFacets" : false,
             "textDescription" : "",
-            "customSearchGroups" : [ {
-                "id" : "group1",
-                "customSearchParameters" : [ {
-                    "list" : [ "a good option", "a bad option", "a so-so option" ],
-                    "id" : "dropdown1",
-                    "displayLabel" : "It's a dropdown"
-                }, {
-                    "defaultValue" : "",
-                    "id" : "field1",
-                    "displayLabel" : "How are you feeling today?"
-                }, {
-                    "date" : 1340895610082,
-                    "id" : "date1",
-                    "displayLabel" : "When it started"
-                }, {
-                    "date" : 1372431610082,
-                    "id" : "date2",
-                    "displayLabel" : "When it ended"
-                } ],
-                "displayLabel" : "Group 1"
-            }, {
-                "id" : "group2",
-                "customSearchParameters" : [ {
-                    "defaultValue" : true,
-                    "id" : "check1",
-                    "displayLabel" : "Make it good"
-                }, {
-                    "defaultValue" : false,
-                    "id" : "check2",
-                    "displayLabel" : "Make it better than that"
-                }, {
-                    "radioOptions" : [ {
-                        "id" : "chicken",
-                        "displayLabel" : "Chicken"
-                    }, {
-                        "id" : "turkey",
-                        "displayLabel" : "Turkey"
-                    }, {
-                        "id" : "roastbeef",
-                        "displayLabel" : "Roast Beef"
-                    } ],
-                    "id" : "radio1",
-                    "displayLabel" : "Which do you prefer?"
-                }, {
-                    "list" : [ "score", "coolness", "price", "length" ],
-                    "id" : "savannaSortOrder",
-                    "displayLabel" : "How do you want to sort it?"
-                } ],
-                "displayLabel" : "Group 2"
-            }, {
-                "id" : "group3",
-                "customSearchParameters" : [ {
-                    "list" : [ "name", "country", "company", "type" ],
-                    "id" : "keyvalues1",
-                    "displayLabel" : "Set some filters"
-                } ],
-                "displayLabel" : "Group 3"
-            } ]
+            "customSearchGroups" : [ ]
 
         }
     ]
