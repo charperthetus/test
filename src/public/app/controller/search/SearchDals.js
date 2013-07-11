855c26bd20cb4c90492dd0f5ce258220098ae883
thille <thille@thetus.com>
2013-07-11 15:15:26 -0700

    BUG: added a second grouped DAL and it breaks the hide/show

--- src/public/app/controller/search/SearchDals.js
+++ src/public/app/controller/search/SearchDals.js
@@ -37,7 +37,7 @@ Ext.define('Savanna.controller.search.SearchDals', {
     },
 
     init: function (app) {
-        this.getDalSourcesStore().loadRawData(this.data2);
+        this.getDalSourcesStore().loadRawData(this.data);
 
         this.control({
             'search_searchdals': {
@@ -556,6 +556,116 @@ Ext.define('Savanna.controller.search.SearchDals', {
                             "displayLabel": "Group 3"
                         }
                     ]
+            },
+            {
+                "id": "MOCK2",
+                "inputTypes": [
+                    {
+                        "type": "sav_searchInputType_Text"
+                    },
+                    {
+                        "type": "sav_searchInputType_FacetFilter"
+                    }
+                ],
+                "outputTypes": [
+                    {
+                        "type": "sav_searchOutputFlagType_ReturnResults"
+                    },
+                    {
+                        "type": "sav_searchOutputFlagType_ReturnFacets"
+                    }
+                ],
+                "displayName": "MOCK",
+                "facetDescriptions": [ ],
+                "timeoutMillis": 5000,
+                "sortOrderVOs": null,
+                "searchGeoTypes": null,
+                "supportsHyperDynamicFacets": false,
+                "textDescription": "A MOCK Dal For Testing Custom Search Options",
+
+                /* WARNING, WARNING, WARNING!!!!!!
+                 We restructured the data to get rid of the parent "customSearchDescription" data-member since it
+                 only has "customSearchGroups" and we do not really want to create a has-a relationship for a model
+                 that is just a wrapper for a has-many....
+                 */
+                "customSearchGroups": [
+                    {
+                        "id": "group1",
+                        "customSearchParameters": [
+                            {
+                                "list": [ "a good option", "a bad option", "a so-so option" ],
+                                "id": "dropdown1",
+                                "displayLabel": "It's a dropdown"
+                            },
+                            {
+                                "defaultValue": "",
+                                "id": "field1",
+                                "displayLabel": "How are you feeling today?"
+                            },
+                            {
+                                "date": 1340895610082,
+                                "id": "date1",
+                                "displayLabel": "When it started"
+                            },
+                            {
+                                "date": 1372431610082,
+                                "id": "date2",
+                                "displayLabel": "When it ended"
+                            }
+                        ],
+                        "displayLabel": "Group 1"
+                    },
+                    {
+                        "id": "group2",
+                        "customSearchParameters": [
+                            {
+                                "defaultValue": true,
+                                "id": "check1",
+                                "displayLabel": "Make it good"
+                            },
+                            {
+                                "defaultValue": false,
+                                "id": "check2",
+                                "displayLabel": "Make it better than that"
+                            },
+                            {
+                                "radioOptions": [
+                                    {
+                                        "id": "chicken",
+                                        "displayLabel": "Chicken"
+                                    },
+                                    {
+                                        "id": "turkey",
+                                        "displayLabel": "Turkey"
+                                    },
+                                    {
+                                        "id": "roastbeef",
+                                        "displayLabel": "Roast Beef"
+                                    }
+                                ],
+                                "id": "radio1",
+                                "displayLabel": "Which do you prefer?"
+                            },
+                            {
+                                "list": [ "score", "coolness", "price", "length" ],
+                                "id": "savannaSortOrder",
+                                "displayLabel": "How do you want to sort it?"
+                            }
+                        ],
+                        "displayLabel": "Group 2"
+                    },
+                    {
+                        "id": "group3",
+                        "customSearchParameters": [
+                            {
+                                "list": [ "name", "country", "company", "type" ],
+                                "id": "keyvalues1",
+                                "displayLabel": "Set some filters"
+                            }
+                        ],
+                        "displayLabel": "Group 3"
+                    }
+                ]
             }
         ]
     },
