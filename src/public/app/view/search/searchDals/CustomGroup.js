8f59f77092500143f7574dcd41547103a9ea7642
thille <thille@thetus.com>
2013-07-12 09:52:36 -0700

    Get select menu working

--- src/public/app/view/search/searchDals/CustomGroup.js
+++ src/public/app/view/search/searchDals/CustomGroup.js
@@ -2,6 +2,10 @@ Ext.define('Savanna.view.search.searchDals.CustomGroup', {
     extend: 'Ext.form.Panel',
     alias: 'widget.searchDals_custom-group',
 
+    requires: [
+        'Ext.form.field.ComboBox'
+    ],
+
     // NOTE: Been trying to figure out how to configure the collapse to be left-aligned and this appears to be the best candidate (though it is all CSS)
     //       http://www.sencha.com/forum/showthread.php?91512-moving-toggle-button-to-the-left
     collapsible: true,
@@ -22,14 +26,32 @@ Ext.define('Savanna.view.search.searchDals.CustomGroup', {
             this.title = this.model.get('displayLabel');
 
             this.model.customSearchParameters().each(function(record) {
-               me.items.push({
-                   xtype: 'textfield',
-                   fieldLabel: record.get('displayLabel'),
-                   name: record.get('id')
-               });
+               me.items.push(me.buildFormXtype(record));
             });
         }
 
         this.callParent(arguments);
+    },
+
+    // CUSTOM METHODS
+
+    buildFormXtype: function(record) {
+        var config = {
+            xtype: 'textfield',
+            fieldLabel: record.get('displayLabel'),
+            name: record.get('id')
+        },
+        list = record.get('list');
+
+        if (list) {
+            config.xtype = 'combobox';
+            config.store = Ext.create('Ext.data.Store', { fields: ['name','value'], data: list.map(function(choice){return {name:choice,value:choice}}) });
+            config.valueField = 'value';
+            config.displayField = 'value';
+            config.forceSelection = true;
+            config.queryMode = 'local';
+        }
+
+        return config;
     }
 });
