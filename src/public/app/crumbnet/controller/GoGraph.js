48d1285264bb856612ca202efe5a52ad29565fc8
kholmanthetus <kholman@thetus.com>
2013-07-22 09:06:21 -0700

    Make layout and alignment menus and add zoomin, zoomout, zoomtofit, undo, redo, grid and overview toolbuttons.  Hooked up zoomtofit, undo, redo, and grid.

--- src/public/app/crumbnet/controller/GoGraph.js
+++ src/public/app/crumbnet/controller/GoGraph.js
@@ -25,54 +25,86 @@ Ext.define('Savanna.crumbnet.controller.GoGraph', {
             },
             'go-graph button': {
                 click: function(button, evt) {
-                    var diagram = button.up('go-graph').down('go-graph_canvas').diagram,
-                        layout = null,
-                        align = null;
+                    var diagram = button.up('go-graph').down('go-graph_canvas').diagram
 
                     switch (button.type) {
-                        case 'tree':
-                            layout = go.GraphObject.make(go.TreeLayout,{ isOngoing: false });
+                        case 'zoomIn':
+                            break;
+                        case 'zoomOut':
+                            break;
+                        case 'zoomToFit':
+                            diagram.zoomToFit();
+                            break;
+                        case 'undo':
+                            diagram.undoManager.undo();
+                            break;
+                        case 'redo':
+                            diagram.undoManager.redo();
                             break;
                         case 'grid':
-                            layout = go.GraphObject.make(go.GridLayout, { comparer: go.GridLayout.smartComparer, isOngoing: false });
-                            break;
-                        case 'force':
-                            layout = go.GraphObject.make(go.ForceDirectedLayout, { isOngoing: false });
-                            break;
-                        case 'right':
-                            align = go.Spot.Right;
-                            break;
-                        case 'left':
-                            align = go.Spot.Left;
-                            break;
-                        case 'top':
-                            align = go.Spot.Top;
-                            break;
-                        case 'bottom':
-                            align = go.Spot.Bottom;
-                            break;
-                        case 'center':
-                            align = go.Spot.Center;
-                            break;
-                        case 'circular':
-                            layout = go.GraphObject.make(go.CircularLayout, { isOngoing: false });
-                            break;
-                        case 'layeredDigraph':
-                            layout = go.GraphObject.make(go.LayeredDigraphLayout, { isOngoing: false, layerSpacing: 50 });
-                            break;
-                        default:
-                            console.error('unknown type (' + button.type + ')');
-                            break;
-                    }
+                            var newSetting = !diagram.grid.visible;
+                            diagram.grid.visible = newSetting;
+                            diagram.toolManager.draggingTool.isGridSnapEnabled = newSetting;
+                            diagram.toolManager.resizingTool.isGridSnapEnabled = newSetting;
 
-                    diagram.startTransaction('ChangeLayout');
-                    if (layout) diagram.layout = layout;
-                    if (align) {
-                        diagram.contentAlignment = align;
-                        diagram.contentAlignment = go.Spot.Default;
+                            //TODO - The diagram is not auto updating itself when I turn the grid on the first time.  It does once it has been shown once.
+                            diagram.update(); //this is not working - try something else
+                            break;
+                        case 'overview':
+                            break;
                     }
-                    diagram.commitTransaction('ChangeLayout');
                 }
+            },
+            'go-graph menu':{click:function(menu, item, event){
+                console.log('clicked menu item', menu, item, event);
+
+                var diagram = menu.up('go-graph').down('go-graph_canvas').diagram,
+                    layout = null,
+                    align = null;
+
+                switch (item.type) {
+                    case 'tree':
+                        layout = go.GraphObject.make(go.TreeLayout,{ isOngoing: false });
+                        break;
+                    case 'grid':
+                        layout = go.GraphObject.make(go.GridLayout, { comparer: go.GridLayout.smartComparer, isOngoing: false });
+                        break;
+                    case 'force':
+                        layout = go.GraphObject.make(go.ForceDirectedLayout, { isOngoing: false });
+                        break;
+                    case 'right':
+                        align = go.Spot.Right;
+                        break;
+                    case 'left':
+                        align = go.Spot.Left;
+                        break;
+                    case 'top':
+                        align = go.Spot.Top;
+                        break;
+                    case 'bottom':
+                        align = go.Spot.Bottom;
+                        break;
+                    case 'center':
+                        align = go.Spot.Center;
+                        break;
+                    case 'circular':
+                        layout = go.GraphObject.make(go.CircularLayout, { isOngoing: false });
+                        break;
+                    case 'layeredDigraph':
+                        layout = go.GraphObject.make(go.LayeredDigraphLayout, { isOngoing: false, layerSpacing: 50 });
+                        break;
+                    default:
+                        console.error('unknown type (' + button.type + ')');
+                        break;
+                }
+                diagram.startTransaction('ChangeLayout');
+                if (layout) diagram.layout = layout;
+                if (align) {
+                    diagram.contentAlignment = align;
+                    diagram.contentAlignment = go.Spot.Default;
+                }
+                diagram.commitTransaction('ChangeLayout');
+            }
             }
         });
     },
