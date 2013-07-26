0ee77e0e58c93b944e20356f217cdb0591277cfa
thille <thille@thetus.com>
2013-07-26 11:46:01 -0700

    Model/Store for palette menu

---
+++ src/public/app/crumbnet/model/TemplateGroup.js
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: thille
+ * Date: 7/25/13
+ * Time: 5:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+/* Ext: false */
+Ext.define('Savanna.crumbnet.model.TemplateGroup', {
+    extend: 'Ext.data.Model',
+
+    requires: [
+        'Savanna.crumbnet.model.Template'
+    ],
+
+    // NOTE: the model class must be fully qualififed in your relationship definition
+    hasMany: [
+        { model: 'Savanna.crumbnet.model.Template', name: 'templates' }
+    ]
+});
