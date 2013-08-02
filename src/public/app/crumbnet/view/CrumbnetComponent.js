3a1cb0aa7615719a95203b188ca10e602bdd2eca
thille <thille@thetus.com>
2013-08-02 16:45:08 -0700

    Use the LinkMapTemplate to generate the list of choices for the menu

--- src/public/app/crumbnet/view/CrumbnetComponent.js
+++ src/public/app/crumbnet/view/CrumbnetComponent.js
@@ -15,50 +15,11 @@ Ext.define('Savanna.crumbnet.view.CrumbnetComponent', {
         type: 'hbox'
     },
 
-    tbar: [
-        {
-            xtype: 'button',
-            itemId: 'layoutMenu',
-            text: 'Layout',
-            menu: [{ type: 'grid', text: 'Grid' },
-                { type: 'tree', text: 'Tree' },
-                { type: 'force', text: 'Force' },
-                { type: 'layeredDigraph', text: 'Layered Digraph' },
-                { type: 'circular', text: 'Circular' }
-            ]
-        },
-        {
-            xtype: 'button',
-            itemId: 'alignmentMenu',
-            text: 'Alignment',
-            menu: [{ type: 'right', text: 'Right' },
-                { type: 'left', text: 'Left' },
-                { type: 'top', text: 'Top' },
-                { type: 'bottom', text: 'Bottom' },
-                { type: 'center', text: 'Center' }
-            ]
-        },
-        {
-            xtype: 'button',
-            itemId: 'linkStyleMenu',
-            text: 'Link Style',
-            menu: [
-                { type: 'line', text: 'Line' },
-                { type: 'curve', text: 'Curve' }
-            ]
-        },
-        '->',
-        {type: 'zoomIn', icon:'resources/images/zoom_in.png', tooltip: 'Zoom In'},
-        {type: 'zoomOut', icon:'resources/images/zoom_out.png', tooltip: 'Zoom Out'},
-        {type: 'zoomToFit', icon:'resources/images/show_all.png', tooltip: 'Zoom To Fit'},
-        {type: 'undo', icon:'resources/images/undo.png', tooltip: 'Undo'},
-        {type: 'redo', icon:'resources/images/redo.png', tooltip: 'Redo'},
-        {type: 'grid', icon:'resources/images/gridview.png', tooltip: 'Toggle Grid'},
-        {type: 'overview', icon:'resources/images/globe.png', tooltip: 'Toggle Overview'}
-    ],
+    tbar: [],
 
     initComponent: function() {
         this.items = this.setupItems();
+        this.tbar = this.setupTbar();
 
         this.callParent(arguments);
     },
@@ -89,6 +50,49 @@ Ext.define('Savanna.crumbnet.view.CrumbnetComponent', {
                     // NOTE: there is a dynamically added "go-graph_overview" component that is managed at runtime
                 ]
             }
+        ];
+    },
+
+    setupTbar: function() {
+        return [
+            {
+                xtype: 'button',
+                itemId: 'layoutMenu',
+                text: 'Layout',
+                menu: [{ type: 'grid', text: 'Grid' },
+                    { type: 'tree', text: 'Tree' },
+                    { type: 'force', text: 'Force' },
+                    { type: 'layeredDigraph', text: 'Layered Digraph' },
+                    { type: 'circular', text: 'Circular' }
+                ]
+            },
+            {
+                xtype: 'button',
+                itemId: 'alignmentMenu',
+                text: 'Alignment',
+                menu: [{ type: 'right', text: 'Right' },
+                    { type: 'left', text: 'Left' },
+                    { type: 'top', text: 'Top' },
+                    { type: 'bottom', text: 'Bottom' },
+                    { type: 'center', text: 'Center' }
+                ]
+            },
+            {
+                xtype: 'button',
+                itemId: 'linkStyleMenu',
+                text: 'Link Style',
+                menu: Ext.Array.map(Savanna.crumbnet.utils.ViewTemplates.getLinkTemplateNames(), function(item, index, array) {
+                    return { type: item, text: item };
+                })
+            },
+            '->',
+            {type: 'zoomIn', icon:'resources/images/zoom_in.png', tooltip: 'Zoom In'},
+            {type: 'zoomOut', icon:'resources/images/zoom_out.png', tooltip: 'Zoom Out'},
+            {type: 'zoomToFit', icon:'resources/images/show_all.png', tooltip: 'Zoom To Fit'},
+            {type: 'undo', icon:'resources/images/undo.png', tooltip: 'Undo'},
+            {type: 'redo', icon:'resources/images/redo.png', tooltip: 'Redo'},
+            {type: 'grid', icon:'resources/images/gridview.png', tooltip: 'Toggle Grid'},
+            {type: 'overview', icon:'resources/images/globe.png', tooltip: 'Toggle Overview'}
         ];
     }
 
