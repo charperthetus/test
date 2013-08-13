2d7e12d3fb9384c2946abd17f3909419620c8107
Joel Griffith <joelgriffith@jgriffith.thetuscorp.com>
2013-08-13 11:00:42 -0700

    SAV-4610 Removing the overrides since it's not relevant on this branch

--- src/public/packages/search-skin/overrides/panel/Tool.js
+++
@@ -1,9 +0,0 @@
-Ext.define('SearchSkin.panel.Tool', {
-	
-	override: 'Ext.panel.Tool',
-
-	renderTpl: [
-        '<img id="{id}-toolEl" src="{blank}" class="{baseCls}-img {baseCls}-{type}' +
-            '{childElCls}" role="presentation" data-wat="wat"/>'
-    ]
-});
