5401ad04e61d6f22b26c27083cdb156164ca7dcf
thille <thille@thetus.com>
2013-08-13 17:35:52 -0700

    removed duplicate code, extraneous comments, and made a comment for UiKit folk

--- src/public/app/crumbnet/view/part/PaletteGroup.js
+++ src/public/app/crumbnet/view/part/PaletteGroup.js
@@ -1,21 +1,13 @@
-/**
- * Created with IntelliJ IDEA.
- * User: thille
- * Date: 7/25/13
- * Time: 5:35 PM
- * To change this template use File | Settings | File Templates.
- */
 /* global Ext: false, go: false, Savanna: false, go: false */
 Ext.define('Savanna.crumbnet.view.part.PaletteGroup', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.crumbnet_part_palette-group',
 
-    model: null,
-
     requires: [
         'Savanna.crumbnet.utils.ViewTemplates'
     ],
 
+    model: null,
     palette: null,
 
     initComponent: function() {
@@ -24,12 +16,12 @@ Ext.define('Savanna.crumbnet.view.part.PaletteGroup', {
         this.callParent(arguments);
 
         this.mon(this, 'expand', function() { this.requestPaletteUpdate(); }, this);
-        this.mon(this, 'expand', function() { this.requestPaletteUpdate(); }, this);
     },
 
     onRender: function() {
         this.callParent(arguments);
 
+        // NOTE: UiKit people, I assume we do not want hard-coded style attributes, but I put them in for the time being
         var domElem = Ext.DomHelper.insertHtml('afterBegin', this.body.dom, '<div class="go-graph-palette" id="palette-' + Ext.id() + '" style="width: 100%; height: 100%;"></div>');
 
         this.palette = new go.Palette(domElem);
