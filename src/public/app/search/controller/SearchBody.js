021f4abad37a4662927fda851c9a71607a5c7bb8
swatson <rufinitsw@hotmail.com>
2013-08-14 10:42:28 -0700

    SAV-3867 Location: Zoom to Polygon and Zoom To World are now working

---
+++ src/public/app/search/controller/SearchBody.js
@@ -0,0 +1,21 @@
+/* global Ext: false */
+Ext.define('Savanna.search.controller.SearchBody', {
+    extend: 'Ext.app.Controller',
+
+    requires: [
+        'Savanna.search.controller.SearchDals'
+    ],
+
+    models: [
+        // coming soon...
+    ],
+    stores: [
+        // coming soon...
+    ],
+
+    views: [
+        'Savanna.search.view.SearchBody'
+    ]
+
+
+});
