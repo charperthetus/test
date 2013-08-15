3014c461590afccd97c8574e76de5a028d8bd094
thille <thille@thetus.com>
2013-08-14 18:34:52 -0700

    test error conditions

--- src/public/test/specs/PrintModalSpec.js
+++ src/public/test/specs/PrintModalSpec.js
@@ -83,5 +83,57 @@ describe('Savanna.view.PrintModal', function() {
                 expect(modal.getIframeContent()).toBe('SOME CONTENT');
             });
         });
-    })
+    });
+
+    describe('accessing the iframe', function() {
+
+        beforeEach(function() {
+            modal = Ext.create('Savanna.view.PrintModal');
+            modal.show();
+        });
+
+        it('should be able to retrieve the iframe document', function() {
+            var iframeDocument = modal.getIframe();
+
+            expect(iframeDocument).not.toBeNull();
+        });
+
+        describe('error conditions', function() {
+            var origErrorHandler,
+                raisedError = false;
+
+            beforeEach(function() {
+                origErrorHandler = Ext.Error.handle;
+
+                Ext.Error.handle = function() {
+                    raisedError = true;
+                    return true;
+                };
+            });
+
+            afterEach(function() {
+                Ext.Error.handle = origErrorHandler;
+
+                raisedError = false;
+            });
+
+            it('should raise an error if the iframe has no id', function() {
+                var iframe = Ext.dom.Query.selectNode('iframe', modal.getEl().dom);
+
+                iframe.setAttribute('name', '');
+
+                modal.getIframe();
+
+                expect(raisedError).toBeTruthy();
+            });
+
+            it('should raise an error if there is no iframe', function() {
+                modal.removeAll();
+
+                modal.getIframe();
+
+                expect(raisedError).toBeTruthy();
+            });
+        });
+    });
 });
