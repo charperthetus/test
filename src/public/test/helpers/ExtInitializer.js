046f36e46634fd21c5ed96d4c0c1c19071b5fe7b
thille <thille@thetus.com>
2013-08-26 12:25:32 -0700

    reverting a change

--- src/public/test/helpers/ExtInitializer.js
+++ src/public/test/helpers/ExtInitializer.js
@@ -1,7 +1,7 @@
 /* global Ext: false, console: false */
 Ext.Loader.setConfig({
     enabled: true,
-    syncModeEnabled: false,
+    syncModeEnabled: true,
     disableCaching: false // NOTE: A great example of a confusing option "disableCaching is false when we do not want to prevent caching...":)
 });
 Ext.application({
