868b16ed8be35c34768594634db5b3782d3ed1f3
thille <thille@thetus.com>
2013-08-28 17:14:17 -0700

    Added cobertura report generation and moved reports into target

--- src/public/test/jscover.sh
+++ src/public/test/jscover.sh
@@ -6,15 +6,16 @@
 
 ROOTDIR="$( cd "$( dirname "$0")" && pwd )"
 COMMAND=$1
+REPORT_DIR=$2
 
 if [[ $COMMAND == "start" ]]; then
-	echo 'Starting JSCover in background...'
+	echo "Starting JSCover in background with reportdir $REPORT_DIR ..."
 
     # NOTE: this assumes we are running in ${project.basedir}/src  If not, all these directories are broken
 
     # DEVOPS: this path for the JAR will likely need to be updated..
     # NOTE: the flexpaper code is currently being left out of instrumentation as it is not being actively worked...
-	java -jar ~/.m2/repository/com/github/tntim96/JSCover/0.3.0/JSCover-0.3.0.jar -ws --document-root=public --report-dir=public/test/reports --no-instrument=ext --no-instrument=resources --no-instrument=test --no-instrument=app/flexpaper > $ROOTDIR/jscover.out 2> $ROOTDIR/jscover.err < /dev/null &
+	java -jar ~/.m2/repository/com/github/tntim96/JSCover/0.3.0/JSCover-0.3.0.jar -ws --document-root=public --report-dir=../target --no-instrument=ext --no-instrument=resources --no-instrument=test --no-instrument=app/flexpaper > $ROOTDIR/jscover.out 2> $ROOTDIR/jscover.err < /dev/null &
 
 	echo $! > $ROOTDIR/jscover.pid
 fi
@@ -31,8 +32,11 @@ if [[ $COMMAND == "stop" ]]; then
 
     echo 'Creating Cobertura reports...'
 
-    # TODO: the path to tests and results are hard-coded...
-  	java -cp ~/.m2/repository/com/github/tntim96/JSCover/0.3.0/JSCover-0.3.0.jar jscover.report.Main --format=COBERTURAXML public/test/reports/jasmine public/app > $ROOTDIR/jscover-report.out 2> $ROOTDIR/jscover-report.err < /dev/null &
+    if [ ! -d "$REPORT_DIR" ]; then
+        mkdir -p $REPORT_DIR
+    fi
+
+  	java -cp ~/.m2/repository/com/github/tntim96/JSCover/0.3.0/JSCover-0.3.0.jar jscover.report.Main --format=COBERTURAXML $REPORT_DIR $REPORT_DIR/original-src/app > $ROOTDIR/jscover-report.out 2> $ROOTDIR/jscover-report.err < /dev/null &
 
     echo 'Cleaning up JSCover report files...'
 
