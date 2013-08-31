d66edb4935c04a34148c0ad026c4fefdf4fa9a78
thille <thille@thetus.com>
2013-08-30 17:11:39 -0700

    Basic JsCover/Unit test working via Ant tasks
    TODO: get cobertura generation to work

--- src/public/test/lib/run-jscover-jasmine.js
+++ src/public/test/lib/run-jscover-jasmine.js
@@ -1,3 +1,4 @@
+/* global require: false, console: false, phantom: false, jscoverage_report: false */
 /**
  * run-jscover-jasmine.js
  *
@@ -13,10 +14,10 @@ var MAX_TIMEOUT = 12 * 60 * 1000; // hopefully our tests don't take longer than 
  * on a server response or for a ui change (fadeIn, etc.) to occur.
  *
  * @param testFx javascript condition that evaluates to a boolean,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
+ * it can be passed in as a string (e.g.: '1 == 1' or '$('#bar').is(':visible')' or
  * as a callback function.
  * @param onReady what to do when testFx condition is fulfilled,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
+ * it can be passed in as a string (e.g.: '1 == 1' or '$('#bar').is(':visible')' or
  * as a callback function.
  * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
  */
@@ -24,74 +25,104 @@ function waitFor(testFx, onReady, timeOutMillis) {
     var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3001, //< Default Max Timeout is 3s
         start = new Date().getTime(),
         condition = false,
-        interval = setInterval(function() {
-            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+        interval = setInterval(function () {
+            if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
                 // If not time-out yet and condition not yet fulfilled
-                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+                condition = (typeof(testFx) === 'string' ? eval(testFx) : testFx()); //< defensive code
             } else {
-                if(!condition) {
+                if (!condition) {
                     // If condition still not fulfilled (timeout but condition is 'false')
-                    console.log("'waitFor()' timeout");
+                    console.log('waitFor() timeout');
                     phantom.exit(1);
                 } else {
                     // Condition fulfilled (timeout and/or condition is 'true')
-                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    console.log('waitFor() finished in ' + (new Date().getTime() - start) + 'ms.');
+                    typeof(onReady) === 'string' ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
                     clearInterval(interval); //< Stop this interval
                 }
             }
         }, 100); //< repeat check every 100ms
-};
-
+}
 
 if (system.args.length !== 2) {
     console.log('Usage: run-jasmine.js URL');
     phantom.exit(1);
 }
 
-var page = require('webpage').create();
+var page = require('webpage').create(),
+    pageUrl = system.args[1];
 
-// Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
-page.onConsoleMessage = function(msg) {
+// Route 'console.log()' calls from within the Page context to the main Phantom context (i.e. current 'this')
+page.onConsoleMessage = function (msg) {
     console.log(msg);
 };
 
-page.open(system.args[1], function(status){
-    if (status !== "success") {
-        console.log("Unable to access network");
+page.open(pageUrl, function (status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
         phantom.exit();
-    } else {
-        waitFor(function(){
-            return page.evaluate(function(){
-                return document.body.querySelector('.symbolSummary .pending') === null
+    }
+    else {
+        console.log('loaded ' + pageUrl);
+
+        waitFor(function () {
+            return page.evaluate(function () {
+                return document.body.querySelector('.symbolSummary .pending') === null;
             });
-        }, function(){
-            var exitCode = page.evaluate(function(){
-                //console.log('');
-                //console.log(document.body.querySelector('.description').innerText);
+        }, function () {
+            var exitCode = page.evaluate(function () {
+                var bodyDesc = document.body.querySelector('.description');
+
+                if (bodyDesc && bodyDesc.innerText) {
+                    console.log('');
+                    console.log(bodyDesc.innerText);
+                }
+
                 var list = document.body.querySelectorAll('.results > #details > .specDetail.failed');
+
                 if (list && list.length > 0) {
-                  //console.log('');
-                  //console.log(list.length + ' test(s) FAILED:');
-                  for (i = 0; i < list.length; ++i) {
-                      var el = list[i],
-                          desc = el.querySelector('.description'),
-                          msg = el.querySelector('.resultMessage.fail');
-                      //console.log('');
-                      //console.log(desc.innerText);
-                      //console.log(msg.innerText);
-                      //console.log('');
-                  }
-                  return 1;
-                } else {
-                  //console.log(document.body.querySelector('.alert > .passingAlert.bar').innerText);
-                  return 0;
+                    console.log('');
+                    console.log(list.length + ' test(s) FAILED:');
+
+                    for (var i = 0; i < list.length; ++i) {
+                        var el = list[i],
+                            desc = el.querySelector('.description'),
+                            msg = el.querySelector('.resultMessage.fail');
+
+                        if (desc || msg) {
+                            console.log('');
+
+                            if (desc && desc.innerText) {
+                                console.log(desc.innerText);
+                            }
+
+                            if (msg && msg.innerText) {
+                                console.log(msg.innerText);
+                            }
+
+                            console.log('');
+                        }
+                    }
+
+                    return 1;
+                }
+                else {
+                    var passingAlert = document.body.querySelector('.alert > .passingAlert.bar');
+
+                    if (passingAlert && passingAlert.innerText) {
+                        console.log(passingAlert.innerText);
+                    }
+
+                    return 0;
                 }
             });
-            page.evaluate(function(){
+
+            page.evaluate(function () {
                 jscoverage_report('phantom');
             });
+
             phantom.exit(exitCode);
+
         }, MAX_TIMEOUT);
     }
 });
