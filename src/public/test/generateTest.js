b9279394bae5e752a3544c245ad9618f13e997fd
thille <thille@thetus.com>
2013-09-23 17:03:04 -0700

    Basic, usable spec generator

--- src/public/test/generateTest.js
+++ src/public/test/generateTest.js
@@ -1,3 +1,4 @@
+/*jshint node: true */
 var readline = require('readline'),
     rl = readline.createInterface({
         input: process.stdin,
@@ -13,18 +14,56 @@ var readline = require('readline'),
         isViewTest: false,
         isControllerTest: false,
         isStoreTest: false,
+        fixture: '',
         requires: []
     };
 
 async.series([
     function(callback) {
-        rl.question('What module do you want to test?', function(answer) {
-            templateData.moduleBeingTested = answer;
+        rl.question('What module do you want to test? ', function(answer) {
+            if (answer) {
+                templateData.moduleBeingTested = answer.trim();
+                callback();
+            }
+            else {
+                callback('you must at least specify the module you want to test...');
+            }
+        });
+    },
+    function(callback) {
+        rl.question('Will this test a view? ', function(answer) {
+            templateData.isViewTest = answer && answer.match(/^y(es)$/i);
+            callback();
+        });
+    },
+    function(callback) {
+        rl.question('Will this a store? ', function(answer) {
+            templateData.isStoreTest = answer && answer.match(/^y(es)$/i);
+            callback();
+        });
+    },
+    function(callback) {
+        rl.question('What fixture would you like to use? (leave it blank if you do not need a fixture) ', function(answer) {
+            if (answer) {
+                templateData.fixture = answer.trim();
+            }
+            callback();
+        });
+    },
+    function(callback) {
+        rl.question('Add any additional libraries to require (separated by commas): ', function(answer) {
+           templateData.requires = answer.trim().split(/\s*,\s*/).filter(function(require) { return require; });
             callback();
         });
     }
-], function(err, results) {
-    var renderedTemplateStr = ejs.render(templateStr, templateData);
-    console.log(renderedTemplateStr);
+], function(err) {
+    if (err) {
+        console.error('Error: ', err);
+    }
+    else {
+        var renderedTemplateStr = ejs.render(templateStr, templateData);
+        console.log(renderedTemplateStr);
+    }
+
     rl.close();
 });
