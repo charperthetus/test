7361760a9895e3f2ca63afba1698549ec75a9550
thille <thille@thetus.com>
2013-09-25 15:02:24 -0700

    Abstract CORS request functionality into static class for use both in proxies and direct Ajax requests

---
+++ src/public/app/utils/CorsAjax.js
@@ -0,0 +1,26 @@
+/* global Ext: false */
+/**
+ * Savanna.utils.CorsAjax
+ *
+ * Singleton wrapper around Ext.Ajax.request to ensure appropriate CORS parameters are added to an Ext.Ajax.request call
+ *
+ * @see http://docs.sencha.com/extjs/4.2.1/#!/api/Ext.Ajax-method-request for documentation on acceptable parameters
+ */
+Ext.define('Savanna.utils.CorsAjax', {
+    extend: 'Ext.Base',
+    singleton: true,
+
+    requires: [
+        'Ext.Ajax'
+    ],
+
+    request: function(options) {
+
+        Ext.apply(options, {
+            cors: true,
+            withCredentials: true
+        });
+
+        return Ext.Ajax.request(options);
+    }
+});
