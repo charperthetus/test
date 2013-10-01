3f5eae81d02f5f65e8a68dcb8c464b3bb9998590
mfawver <mfawver@thetus.com>
2013-10-01 12:23:42 -0700

    Add URL base for metadata

--- src/public/config/Config.js
+++ src/public/config/Config.js
@@ -28,6 +28,7 @@ SavannaConfig.searchUrl = SavannaConfig.savannaUrlRoot + 'rest/search'; // local
 SavannaConfig.itemViewUrl = SavannaConfig.savannaUrlRoot + 'rest/c2is2/model/item/';
 SavannaConfig.dalSourcesUrl = SavannaConfig.savannaUrlRoot + 'rest/search/sources'; // local dev version: 'resources/data/testSearchDalsWithFormData.json',
 SavannaConfig.locationSearch = SavannaConfig.savannaUrlRoot + '/rest/map/search';   // local dev version: 'resources/data/testSearchLocationSearch.json',
+SavannaConfig.metadataUrl = SavannaConfig.savannaUrlRoot + 'rest/metadata';
 // TODO: replace this test URL with real endpoint once we have one...
 SavannaConfig.crumbnetTemplatesUrl = SavannaConfig.savannaUrlRoot + 'resources/data/testCrumbnetTemplates.json';
 
