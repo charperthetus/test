b19ab1bfc8a560ae00a85aa3f90da045c938c756
jstafford <jstafford@thetus.com>
2013-10-07 15:07:25 -0700

    SAV-5301 - Basic Process Editor with auto layout

--- src/public/app/process/controller/ProcessController.js
+++ src/public/app/process/controller/ProcessController.js
@@ -8,19 +8,34 @@
 Ext.define('Savanna.process.controller.ProcessController', {
     extend: 'Deft.mvc.ViewController',
 
+    requires: [
+        'Savanna.process.utils.ViewTemplates'
+    ],
+
     control: {
-        addstepbutton: {
-            click: "addStepClick"
-        },
-        adddecisionbutton: {
-            click: "addDecisionClick"
-        },
+//        addstepbutton: {
+//            click: 'addStepClick'
+//        },
+//        adddecisionbutton: {
+//            click: 'addDecisionClick'
+//        },
         expandsteps: {
-            click: "expandStepsClick"
+            click: 'expandStepsClick'
         },
         collapsesteps: {
-            click: "collapseStepsClick"
-        }
+            click: 'collapseStepsClick'
+        },
+        loadJSON: {
+            click: 'loadJSONClick'
+        },
+        saveJSON: {
+            click: 'saveJSONClick'
+        },
+        clearJSON: {
+            click: 'clearJSONClick'
+        },
+        canvas: {},
+        metadata: {}
     },
     init: function() {
         return this.callParent(arguments);
@@ -32,10 +47,38 @@ Ext.define('Savanna.process.controller.ProcessController', {
     addDecisionClick: function() {
         console.log('add decision'); //todo: remove console log statement and do something useful
     },
+    toggleExpanded: function(expand) {
+        var canvas = this.getCanvas();
+        var diagram = canvas.diagram;
+        diagram.startTransaction('toggleExpanded');
+        var iter = diagram.nodes;
+        while ( iter.next() ){
+            var node = iter.value;
+            if (node instanceof go.Group) {
+                node.isSubGraphExpanded = expand;
+            }
+        }
+        diagram.commitTransaction('toggleExpanded');
+    },
     expandStepsClick: function() {
-        console.log('expand all steps'); //todo: remove console log statement and do something useful
+        this.toggleExpanded(true);
     },
     collapseStepsClick: function() {
-        console.log('collapse all steps'); //todo: remove console log statement and do something useful
+        this.toggleExpanded(false);
+    },
+    loadJSONClick: function() {
+        var canvas = this.getCanvas();
+        var metadata = this.getMetadata();
+        Savanna.process.utils.ViewTemplates.load(canvas.diagram, metadata.down('#JSONtextarea'));
+    },
+    saveJSONClick: function() {
+        var canvas = this.getCanvas();
+        var metadata = this.getMetadata();
+        Savanna.process.utils.ViewTemplates.save(canvas.diagram, metadata.down('#JSONtextarea'));
+    },
+    clearJSONClick: function() {
+        var canvas = this.getCanvas();
+        var metadata = this.getMetadata();
+        Savanna.process.utils.ViewTemplates.clear(canvas.diagram, metadata.down('#JSONtextarea'));
     }
 });
--- src/public/app/process/view/ProcessEditorComponent.js
+++ src/public/app/process/view/ProcessEditorComponent.js
@@ -11,7 +11,7 @@ Ext.define('Savanna.process.view.ProcessEditorComponent', {
 
     requires: [
         'Ext.layout.container.Border',
-        'Savanna.process.view.part.PaletteWindow',
+//        'Savanna.process.view.part.PaletteWindow',
         'Savanna.process.view.part.Toolbar',
         'Savanna.process.view.part.Canvas', //todo: do we refactor this code?
         'Savanna.process.view.part.MetadataTabPanel'
@@ -48,6 +48,7 @@ Ext.define('Savanna.process.view.ProcessEditorComponent', {
                 items:[
                     {
                         xtype: 'process_canvas', //todo: refactor graph canvas
+                        itemId: 'canvas',
                         width: '100%',
                         height: '100%'
                     },
@@ -60,6 +61,7 @@ Ext.define('Savanna.process.view.ProcessEditorComponent', {
             },
             {
                 xtype: 'process_metadata', //todo - this should be refactored out for all savanna item components later
+                itemId: 'metadata',
                 region: 'east',
                 layout: 'fit',
                 width: '30%', //todo: DI should apply the appropriate styling for the sidebar
