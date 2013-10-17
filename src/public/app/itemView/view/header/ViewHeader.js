2ed71f520dc2678526ef34cd0351e43ff63108c8
bjohnson <bjohnson@thetus.com>
2013-10-17 10:52:31 -0700

    view header looking good

--- src/public/app/itemView/view/header/ViewHeader.js
+++ src/public/app/itemView/view/header/ViewHeader.js
@@ -17,19 +17,47 @@ Ext.define('Savanna.itemView.view.header.HeaderView', {
 
     parentItem: null,
 
+    hideHeaders: true,
+
+    columnTpl: Ext.create('Ext.XTemplate',
+        '<tpl if="label == \"Parent\"">',
+            '<b>{label}</b><p>{value[0].label}</p>',
+        '<tpl else>',
+            '<b>{label}</b>',
+            '<tpl for="values">',
+                '<p>{value}</p>',
+            '</tpl>',
+        '</tpl>'),
+
     columns: [
-        {
-            dataIndex: 'label',
-            sortable: false
-        },
+//        {
+//            dataIndex: 'label',
+//            sortable: false
+//        },
         {
             xtype: 'templatecolumn',
             dataIndex: 'values',
+            tpl: Ext.create('Ext.XTemplate',
+                '<tpl if="label == \'Parent\'">',
+                    '<b>{label}&nbsp;&nbsp;</b>',
+                    '<tpl for="values">',
+                        '{label}',
+                    '</tpl>',
+                '<tpl elseif="label == \'Description\'">',
+                    '<tpl for="values">',
+                        '{value}',
+                    '</tpl>',
+                '<tpl else>',
+                    '<b>{label}&nbsp;&nbsp;</b>',
+                    '<tpl for="values">',
+                        '{value},&nbsp;&nbsp;',
+                    '</tpl>',
+                '</tpl>'),
             flex: 1,
-            tpl: '',
-            renderer: function(value, metaData, record, row, col, store, gridView) {
-                return this.renderValues(value, record);
-            },
+            rowLines: false,
+//            renderer: function(value, metaData, record, row, col, store, gridView) {
+//                return this.renderValues(value, record);
+//            },
             sortable: false
         }
     ],
@@ -38,9 +66,14 @@ Ext.define('Savanna.itemView.view.header.HeaderView', {
         var returnStr = '';
 
         if (record.data.label == 'Parent') {
-            returnStr = value[0].label;
+            returnStr = record.data.label + '\t' + value[0].label;
+        }
+        else if (record.data.label == 'Description') {
+            returnStr = value[0].value;
         }
         else {
+            returnStr = record.data.label + "          ";
+
             Ext.each(value, function(val) {
                 returnStr += val.value + ', ';
             });
