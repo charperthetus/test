045915ec97e68c1254ac50003dfadca19b79d796
jgriffith <jgriffith@thetus.com>
2013-10-17 17:52:12 -0700

    Moving controller logic over

--- src/public/app/itemView/controller/ImageBrowserController.js
+++ src/public/app/itemView/controller/ImageBrowserController.js
@@ -12,8 +12,51 @@ Ext.define('Savanna.itemView.controller.ImageBrowserController', {
         'Savanna.itemView.view.imageBrowser.ImagesThumbnail'
     ],
 
+    control: {
+        nav_left: {
+            click:'onNavLeft'
+        },
+        nav_right: {
+            click: 'onNavRight'
+        }
+    },
+
     init: function() {
-        console.log("Controller loaded");
         return this.callParent(arguments);
-    }
+    },
+
+    // Scroll Left Button
+    onNavLeft: function() {
+        var gallery = Ext.ComponentQuery.query('#thumbnail_list')[0];
+        gallery.scrollBy(-450, 0, true);
+    },
+
+    // Scroll Right Button
+    onNavRight: function() {
+        var gallery = Ext.ComponentQuery.query('#thumbnail_list')[0];
+        gallery.scrollBy(450, 0, true);
+    },
+
+    // Selecting an image to expand
+    onChangeImage: function(btn, image) {
+        var selectedImage = image.src,
+            title = (image.title) ? image.title : 'No title',
+            description = (image.alt) ? image.alt : 'No description',
+            jumboImage = Ext.ComponentQuery.query('#image_primary')[0],
+            jumboMeta = Ext.ComponentQuery.query('#image_text')[0],
+            imageWidth = image.naturalWidth,
+            imageHeight = image.naturalHeight;
+
+        var backgroundSize = (imageWidth < jumboImage.width && imageHeight < jumboImage.height) ? 'inherit' : 'contain';
+        
+        // In order to display text over an image, the image is used as a background image on a panel
+        jumboImage.setBodyStyle({
+            backgroundImage: 'url(' + selectedImage + ')',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center center',
+            backgroundSize: backgroundSize,
+            backgroundColor: 'transparent'
+        });
+        jumboMeta.update(description);
+    },
 });
--- src/public/app/itemView/controller/ItemViewController.js
+++ src/public/app/itemView/controller/ItemViewController.js
@@ -126,41 +126,6 @@ Ext.define('Savanna.itemView.controller.ItemViewController', {
         });
     },
 
-    // Scroll Left Button
-    onNavLeft: function() {
-        var gallery = Ext.ComponentQuery.query('#thumbnail_list')[0];
-        gallery.scrollBy(-450, 0, true);
-    },
-
-    // Scroll Right Button
-    onNavRight: function() {
-        var gallery = Ext.ComponentQuery.query('#thumbnail_list')[0];
-        gallery.scrollBy(450, 0, true);
-    },
-
-    // Selecting an image to expand
-    onChangeImage: function(btn, image) {
-        var selectedImage = image.src,
-            title = (image.title) ? image.title : 'No title',
-            description = (image.alt) ? image.alt : 'No description',
-            jumboImage = Ext.ComponentQuery.query('#image_primary')[0],
-            jumboMeta = Ext.ComponentQuery.query('#image_text')[0],
-            imageWidth = image.naturalWidth,
-            imageHeight = image.naturalHeight;
-
-        var backgroundSize = (imageWidth < jumboImage.width && imageHeight < jumboImage.height) ? 'inherit' : 'contain';
-        
-        // In order to display text over an image, the image is used as a background image on a panel
-        jumboImage.setBodyStyle({
-            backgroundImage: 'url(' + selectedImage + ')',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center center',
-            backgroundSize: backgroundSize,
-            backgroundColor: 'transparent'
-        });
-        jumboMeta.update(description);
-    },
-
     setupProperties: function (data, view) {
         //ToDo: set up to load current incoming properties
     },
