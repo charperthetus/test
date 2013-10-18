ce57b7e72199534b93ddceed19e2317ddb0b06fe
jgriffith <jgriffith@thetus.com>
2013-10-17 17:48:04 -0700

    Updated config for new service

--- src/public/config/Config.js
+++ src/public/config/Config.js
@@ -6,7 +6,7 @@
  */
 SavannaConfig = {
     // Root for all REST calls
-    savannaUrlRoot: 'https://c2devsav1.thetuscorp.com:8443/c2is2/',
+    savannaUrlRoot: 'http://c2devsav1:8080/c2is2/',
     //savannaUrlRoot: 'http://thedevsav1.thetuscorp.com:8080/SavannaX/',
 
     helpUrl: 'http://www.google.com',
