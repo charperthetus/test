bc8c046f3ddb915756df49db61d244f0b0b104e5
jgriffith <jgriffith@thetus.com>
2013-10-21 14:32:34 -0700

    Final tests are passing, code for views is 100% covered

--- src/public/test/specs/ItemViewerSpec.js
+++ src/public/test/specs/ItemViewerSpec.js
@@ -102,7 +102,7 @@ describe('Item Viewer', function () {
 
     // TODO: Test image browser component view when it's built
 
-    describe('Controllers', function() {
+    describe('Main Controller', function() {
         // TODO: Update this when edit mode is in place
         // it('should set the display label', function(){
         //     itemviewController.setupDisplayLabel('Title', itemviewComponent.down('itemview_view_header'));
@@ -163,5 +163,24 @@ describe('Item Viewer', function () {
                 expect(Savanna.app.fireEvent).toHaveBeenCalled();
             });
         });
+    });
+
+    describe('Header View Controller', function() {
+        it('should fire an "open" event when clicking a parent item', function() {
+            var clickEvent = { target: { value: 'title', name: 'uristring', id: 'openParentItem' } };
+            spyOn(Savanna.app, 'fireEvent');
+            itemviewComponent.down('itemview_view_header').getController().openParentItem(null, null, null, null, clickEvent);
+            expect(Savanna.app.fireEvent).toHaveBeenCalled();
+        });
+    });
+
+    describe('Related Processes Controller', function() {
+        it('should fire an "open" event when clicking a process', function() {
+            var clickEvent = { target: { value: 'title', name: 'uristring', id: 'openProcess' } };
+            spyOn(Savanna.app, 'fireEvent');
+            itemviewComponent.down('itemview_related_processes').getController().openRelatedProcesses(null, null, null, null, clickEvent);
+            expect(Savanna.app.fireEvent).toHaveBeenCalled();
+        });
+
     });
 });
