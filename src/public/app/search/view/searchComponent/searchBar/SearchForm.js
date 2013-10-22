e8dd46879b320b683488b7445f308c6107e62981
jamiebarmettler <jbarmettler@thetus.com>
2013-10-21 20:38:18 -0700

    SAV-5634  Removed an accidental change to search.

--- src/public/app/search/view/searchComponent/searchBar/SearchForm.js
+++ src/public/app/search/view/searchComponent/searchBar/SearchForm.js
@@ -106,17 +106,6 @@ Ext.define('Savanna.search.view.searchComponent.searchBar.SearchForm', {
             emptyText: 'Search'
         },
         {
-            xtype: 'button',
-            itemId: 'search_clear',
-            glyph: 61731
-        },
-        {
-            xtype: 'button',
-            itemId: 'search_submit',
-            glyph: 61808
-        },
-        {
-            hidden: true,
             text: 'Advanced',
             itemId: 'searchadvanced_btn'
 
