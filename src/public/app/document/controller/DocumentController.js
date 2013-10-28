68d17e1e1404aba607116370493c156c367a65da
kholman <kholman@thetus.com>
2013-10-27 20:26:17 -0700

    Initial Document View, hooked up to open.

---
+++ src/public/app/document/controller/DocumentController.js
@@ -0,0 +1,99 @@
+Ext.define("Savanna.document.controller.DocumentController", {
+    extend: 'Deft.mvc.ViewController',
+    currentTool: 'hand',
+    dvId: null,
+    control: {
+        searchText: true,
+        handTool: {
+            click: 'onHandTool'
+        },
+        selectTool: {
+            click: 'onSelectTool'
+        },
+        zoomIn: {
+            click: 'onZoomIn'
+        },
+        zoomOut: {
+            click: 'onZoomOut'
+        },
+        zoomFit: {
+            click: 'onZoomFit'
+        },
+        nextPage: {
+            click: 'onNextPage'
+        },
+        previousPage: {
+            click: 'onPreviousPage'
+        },
+        singlePageView: {
+            click: 'onSinglePageView'
+        },
+        thumbView: {
+            click: 'onThumbView'
+        },
+        twoPageView: {
+            click: 'onTwoPageView'
+        },
+        searchDoc: {
+            click: 'onSearchDoc'
+        },
+        exportDoc: {
+            click: 'onExportDoc'
+        },
+        printDoc: {
+            click: 'onPrintDoc'
+        }
+    },
+    init: function () {
+        this.dvId = this.getView().docViewId;
+    },
+    onHandTool: function() {
+        //Ext.get(viewerId).setStyle('cursor', 'pointer');
+        $FlexPaper(this.dvId).setCurrentCursor("ArrowCursor");
+        this.currentTool = "hand";
+    },
+    onSelectTool: function() {
+        $FlexPaper(this.dvId).setCurrentCursor("TextSelectorCursor");
+        this.currentTool = "select";
+    },
+    onZoomIn: function() {
+        $FlexPaper(this.dvId).setZoom($FlexPaper(this.dvId).scale + .2);
+    },
+    onZoomOut: function() {
+        $FlexPaper(this.dvId).setZoom($FlexPaper(this.dvId).scale - .2);
+    },
+    onZoomFit: function() {
+        $FlexPaper(this.dvId).fitHeight();
+        $FlexPaper(this.dvId).fitwidth();
+    },
+    onNextPage : function() {
+        $FlexPaper(this.dvId).nextPage();
+    },
+    onPreviousPage : function() {
+        $FlexPaper(this.dvId).prevPage();
+    },
+    onSinglePageView:function() {
+        $FlexPaper(this.dvId).switchMode("Portrait");
+    },
+    onThumbView:function() {
+        $FlexPaper(this.dvId).switchMode("Tile");
+    },
+    onTwoPageView:function() {
+        $FlexPaper(this.dvId).switchMode("TwoPage");
+    },
+    onSearchDoc : function() {
+        var searchText = this.getSearchText().getValue();
+        if (searchText != "") {
+            $FlexPaper(this.dvId).searchText(searchText);
+        }
+    },
+    onPrintDoc : function() {
+        $FlexPaper(this.dvId).printPaper();
+    },
+    onExportDoc : function() {
+        //TODO - Need Original
+        location.href =  SavannaConfig.savannaUrlRoot + "rest/document/" + this.getView().itemUri + ";jsessionid=" + Savanna.jsessionid
+    }
+
+
+});
