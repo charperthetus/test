1867432326014fbfe52c153cfc6011460b7039b0
Edward@Thetus <edward@castersblues.com>
2013-10-30 12:59:16 -0700

    Fixed new savannah tab and moder search icons

--- src/public/app/desktop/view/SavannaTabPanel.js
+++ src/public/app/desktop/view/SavannaTabPanel.js
@@ -31,10 +31,11 @@ Ext.define('Savanna.desktop.view.SavannaTabPanel', {
 
     setupTabBarItems: function() {
         return [{
-            xtype: 'tab',
-            glyph:'plus',
+            xtype: 'button',
+//            glyph:'plus',
             ui: 'dark',
             width:25,
+            height:25,
             cls:'new-savanna-tab',
             closable: false,
             menu: {
--- src/public/app/search/controller/resultsComponent/ResultsPanelToolbarController.js
+++ src/public/app/search/controller/resultsComponent/ResultsPanelToolbarController.js
@@ -49,13 +49,7 @@ Ext.define('Savanna.search.controller.resultsComponent.ResultsPanelToolbarContro
         var listBtn = this.getView().queryById('results_listViewButton');
         var mapBtn = this.getView().queryById('results_mapViewButton');
 
-        if(listBtn.pressed) {
-            listBtn.setGlyph(61786);
-            mapBtn.setGlyph(61746);
-        } else  {
-            listBtn.setGlyph(61746);
-            mapBtn.setGlyph(61786);
-        }
+
     },
 
     init: function () {
