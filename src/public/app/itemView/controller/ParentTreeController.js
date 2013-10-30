180e59391c00d39a2feb49a0049c6b8b8d8aa06c
ksonger <ken@kensonger.com>
2013-10-30 15:01:21 -0700

    more work on the treepanel, adding in click delay and cleaning up click method

--- src/public/app/itemView/controller/ParentTreeController.js
+++ src/public/app/itemView/controller/ParentTreeController.js
@@ -18,38 +18,97 @@ Ext.define('Savanna.itemView.controller.ParentTreeController', {
 
     control: {
         parentitems_treepanel: {
+
             beforeitemmousedown: 'onBeforeMouseDown',
             itemclick: 'onItemClick',
             beforeitemdblclick: 'onItemDblClick'
+
         }
     },
 
-    selectedItemExpanded:false,
+    selectedItemExpanded: false,
+
+    fetching: false,
+
+    clickDisabled: false,
 
     onItemDblClick: function (view, record) {
         record.isExpandingOrCollapsing = false;
         return false;
     },
 
-    onBeforeMouseDown:function(view, record, item, index, e, eOpts) {
-
+    onBeforeMouseDown: function (view, record) {
         this.selectedItemExpanded = record.data.expanded;
+    },
+
+    clickEnable:function()  {
+        if(!this.fetching)  {
+            this.clickDisabled = false;
+            this.getView().queryById('parentitems_treepanel').getEl().setOpacity(1, true);
+        }
     },
 
     onItemClick: function (view, record, item, index, e, eOpts) {
 
-        this.getView().up('itemview_create_item').selectedParentUri = record.get('uri');
+        if (!this.clickDisabled) {
+            /*
+            slow down, chief - you're literally freaking me out
+             */
+            this.clickDisabled = true;
 
-        Savanna.app.fireEvent('itemview:treepanel:itemclick', view, record, item, index, e, eOpts);
+            Ext.defer(this.clickEnable, 800, this);
 
-        var tree = this.getView().queryById('parentitems_treepanel');
+            var tree = this.getView().queryById('parentitems_treepanel'),
+                selectedNode = tree.getSelectionModel().getSelection()[0] || tree.getRootNode();
 
-        var selectedNode = tree.getSelectionModel().getSelection()[0] || tree.getRootNode();
+            if (record.data.hasChildren) {
+                /*
+                 record can be expanded or collapsed
+                 */
+                if (record.childNodes.length === 0) {
+                    /*
+                     record has not loaded children yet
+                     */
+                    if (!this.selectedItemExpanded) {
+                        /*
+                         record is not expanded, else do nothing
+                         */
+                        if (!this.fetching) {
+                            /*
+                             there isn't a request already active
+                             */
+                            this.fetching = true;
+                            tree.getEl().setOpacity(.7, true);
+                            this.fetchChildItems(record, selectedNode);
+                        }
+                    }
 
-        if (this.selectedItemExpanded) {
-            selectedNode.collapse();
-        } else {
-            this.fetchChildItems(record, selectedNode);
+                } else {
+                    /*
+                     record has children, and they are already loaded
+                     */
+                    if (!this.selectedItemExpanded) {
+                        /*
+                         record is not expanded
+                         */
+                        record.expand();
+                    } else {
+                        /*
+                         record is expanded
+                         */
+                        record.collapse();
+                    }
+                }
+            } else {
+                /*
+                 this must be a leaf
+                 */
+            }
+            /*
+             always show details on the left
+             */
+            this.getView().up('itemview_create_item').selectedParentUri = record.get('uri');
+            Savanna.app.fireEvent('itemview:treepanel:itemclick', view, record, item, index, e, eOpts);
         }
     },
 
@@ -65,36 +124,31 @@ Ext.define('Savanna.itemView.controller.ParentTreeController', {
 
             myStore = Ext.create('Savanna.itemView.store.ParentItemsStore', {
                 storeId: 'itemview_' + record.get('id')
-            })
+            });
             myStore.getProxy().url = uri;
 
             myStore.load({
                 callback: Ext.bind(this.onChildItemsFetched, this, [selectedNode, tree], true)
             });
-        } else {
-
-            if(selectedNode.childNodes.length > 0)  {
-                selectedNode.expand();
-            }
         }
     },
     onChildItemsFetched: function (records, operation, success, selectedNode) {
 
+        selectedNode.removeAll();
+
         Ext.each(records, function (record) {
             record.set('leaf', !record.get('hasChildren'));
-
-            if (selectedNode.isLeaf()) {
-                //insert the node in the parent node
-                selectedNode.parentNode.insertChild(0, record);
-            } else {
-                //inserting as a child
-                selectedNode.insertChild(0, record);
-            }
+            /*
+            inserting as a child
+             */
+            selectedNode.insertChild(0, record);
         });
 
-        if (!selectedNode.isExpanded()) {
-            selectedNode.expand();
-        }
+        selectedNode.expand();
+
+        this.fetching = false;
+
+        this.clickEnable();
     }
 });
 
