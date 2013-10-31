74a584cb2d2005d0f269af86e4bd8b51e8bff909
Edward@Thetus <edward@castersblues.com>
2013-10-31 13:28:26 -0700

    set edit view icon color

--- src/public/app/components/autoComplete/AutoComplete.js
+++ src/public/app/components/autoComplete/AutoComplete.js
@@ -58,7 +58,7 @@ Ext.define('Savanna.components.autoComplete.AutoComplete', {
 
     buildItems: function() {
         // If autocomplete has additional controls, generate a space to insert them.
-        var closeButton = (this.getIsClosable()) ? Ext.create('Ext.button.Button', { text: 'Close', itemId: 'closeautocomplete' }) : {};
+        var closeButton = (this.getIsClosable()) ? Ext.create('Ext.button.Button', { glyph: 'closeRollover', itemId: 'closeautocomplete' }) : {};
 
         return [
             {
