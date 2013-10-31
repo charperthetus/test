dc3f494df740d1e0e9911ad73cb5d6ff1d30272c
lsanz <lsanz@thetus.com>
2013-10-31 13:26:49 -0700

    adding a class to add a single line down the middle of the app for split pane mode.

--- src/public/app/desktop/controller/WorkspaceController.js
+++ src/public/app/desktop/controller/WorkspaceController.js
@@ -58,6 +58,7 @@ Ext.define('Savanna.desktop.controller.WorkspaceController', {
 
     setupSecondaryTabPanel: function() {
         return Ext.create('Savanna.desktop.view.SavannaTabPanel', {
+            cls:'secondPanel',
             itemId: 'secondarytabpanel',
             config: {
                 view: 'split'
