160806163b0c0918758f9ffa02a3e501d2facafa
kholmanthetus <kholman@thetus.com>
2013-11-07 09:50:34 -0800

    Add message for leaving the page to prevent losing unsaved data.

--- src/public/config/Config.js
+++ src/public/config/Config.js
@@ -15,57 +15,22 @@ SavannaConfig = {
     showSystemHighClassification: true,
 
     /**
-     WMS base layer
-     **/
-
-//    mapDefaultBaseLayer: {
-//        url: 'http://demo.opengeo.org/geoserver/wms',
-//        projection: 'EPSG:4326',
-//        type: 'WMS',
-//        layerName: 'bluemarble',
-//        center: '0,0',
-//        zoom: 2
-//    }
-
-    /**
      MapQuest XYZ Tile Layer (Tiles arranged in a standard XYZ grid)
      **/
-
     mapDefaultBaseLayer: {
-    url:  [
-        "http://oatile1.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
-        "http://oatile2.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
-        "http://oatile3.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
-        "http://oatile4.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg"
-    ],
-    projection: 'EPSG:900913',
-    type: 'XYZ',
-    layerName: 'MapQuest Imagery',
-    center: '0,0',
-    zoom: 2
-    }
+        url: [
+            "http://oatile1.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
+            "http://oatile2.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
+            "http://oatile3.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
+            "http://oatile4.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg"
+        ],
+        projection: 'EPSG:900913',
+        type: 'XYZ',
+        layerName: 'MapQuest Imagery',
+        center: '0,0',
+        zoom: 2
+    },
 
-//    mapDefaultBaseLayer: {
-//        url: 'http://geocache.grayhive.org/tms/',
-//        projection: 'EPSG:4326',
-//        type: 'TMS2',
-//        maxResolution: 0.703125,
-//        imgType: 'png',
-//        layerName: 'vmap',
-//        center: '0,0',
-//        zoom: 1
-//    }
-
-
-//    mapDefaultBaseLayer: {
-//        url: 'http://tilecache.osgeo.org/wms-c/Basic.py/',
-//        projection: 'EPSG:4326',
-//        maxResolution: 0.703125,
-//        type: 'TMS',
-//        imgType: 'png',
-//        layerName: 'basic',
-//        center: '0,0',
-//        zoom: 1
-//    }
+    debug: true //Setting just for devs, this doesn't need to be included in the customer config file.
 
 };
