295a560c9b12d7bcecda616edd26faa265369b7f
ScottSeely <sseely@thetus.com>
2013-11-07 09:03:40 -0800

    SAV-6058

--- src/public/app/search/view/searchComponent/searchBody/resultsComponent/resultsDals/ResultsFacet.js
+++ src/public/app/search/view/searchComponent/searchBody/resultsComponent/resultsDals/ResultsFacet.js
@@ -382,7 +382,8 @@ Ext.define('Savanna.search.view.searchComponent.searchBody.resultsComponent.resu
             component = searchController.getSearchComponent(btn),
             currentDalPanel = component.down('#searchdals').queryById(this.dal.get('id')),
             searchString = component.queryById('searchbar').buildSearchString(),
-            searchObj = searchController.buildSearchObject(searchString, this.dal, currentDalPanel);
+            mapView = component.down('search_map_canvas'),
+            searchObj = searchController.buildSearchObject(searchString, this.dal, currentDalPanel, mapView);
 
         searchController.buildAndLoadResultsStore(this.dal, component, searchObj, 'filter');
     }
