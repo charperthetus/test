e0580cefe399263ac773d2d07a84ccab08736bea
kholmanthetus <kholman@thetus.com>
2013-11-09 16:50:04 -0800

    Cleanup spacing.

--- src/public/app/document/controller/DocumentBodyController.js
+++ src/public/app/document/controller/DocumentBodyController.js
@@ -1,54 +1,48 @@
 Ext.define("Savanna.document.controller.DocumentBodyController", {
-	 extend: 'Deft.mvc.ViewController',
-	 
-	 init: function() {
-                
-                //get the view part
-                var myView = this.getView();
+    extend: 'Deft.mvc.ViewController',
 
-                // get the domElement which is the Flexpaper element 
-                var domElement = myView.domElement;
+    init: function () {
 
-                // Getting the toolbar instance
-                var activeToolBar = myView.findParentByType('documentcomponent').down('document_toolbar');
-	
- 		//Bind all flexpaper events
-		jQuery(domElement).bind('onDocumentLoading', function(e) {
-			
-		});
-		
-		jQuery(domElement).bind('onCurrentPageChanged', function(e, pagenum) {
+        //get the view part
+        var myView = this.getView();
 
-                        // setting the current page value in currentPage text field in active tab
-                        activeToolBar.down('#currentPage').setValue(pagenum);
+        // get the domElement which is the Flexpaper element
+        var domElement = myView.domElement;
+
+        // Getting the toolbar instance
+        var activeToolBar = myView.findParentByType('documentcomponent').down('document_toolbar');
+
+        jQuery(domElement).bind('onCurrentPageChanged', function (e, pagenum) {
+
+            // setting the current page value in currentPage text field in active tab
+            activeToolBar.down('#currentPage').setValue(pagenum);
+
+            var prevButton = activeToolBar.down('#previousPage');
+
+            var nextButton = activeToolBar.down('#nextPage');
+
+            //if the page number is one
+            if (pagenum == 1) {
+                prevButton.setDisabled(true);
+                nextButton.setDisabled(false);
+            }
+            //if the page number is end of the page
+            else if (pagenum == $FlexPaper(myView.docViewId).getTotalPages()) {
+                prevButton.setDisabled(false);
+                nextButton.setDisabled(true);
+            }
+            else {
+                prevButton.setDisabled(false);
+                nextButton.setDisabled(false);
+            }
+        });
+
+        jQuery(domElement).bind('onDocumentLoaded', function (e, totalPages) {
+
+            // setting the total page value in TotalPage Label field in active tab
+            activeToolBar.down('#totalPages').setText(totalPages);
+        });
+    }
 
 
-                        var prevButton = activeToolBar.down('#previousPage');
-
-                        var nextButton = activeToolBar.down('#nextPage');
-
-                        //if the page number is one			
-                        if(pagenum == 1) {
-	                        prevButton.setDisabled(true);
-	                        nextButton.setDisabled(false);
-                        }
-                        //if the page number is end of the page			
-                        else if(pagenum == $FlexPaper(myView.docViewId).getTotalPages()) {      
-	                        prevButton.setDisabled(false);
-	                        nextButton.setDisabled(true);
-                        }
-                        else {
-	                        prevButton.setDisabled(false);
-	                        nextButton.setDisabled(false);
-                        }
-		});
-		
-		jQuery(domElement).bind('onDocumentLoaded',function(e, totalPages) {
-
-                        // setting the total page value in TotalPage Label field in active tab
-                        activeToolBar.down('#totalPages').setText(totalPages);
-		});
-	}
-	
-	 
 });
