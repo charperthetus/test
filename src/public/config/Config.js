35927d8fda9fe0e5acb6d8a266502700059b63c5
Alison Stump <alison@alisonstump.com>
2013-11-12 21:06:10 -0800

    Process Editor Details styling

--- src/public/config/Config.js
+++ src/public/config/Config.js
@@ -8,8 +8,8 @@ SavannaConfig = {
 // Root for all REST calls
     
 //    savannaUrlRoot: 'http://c2aptsav1:8080/c2is2/',
-    savannaUrlRoot: 'https://c2astsav1.thetuscorp.com/c2is2/',
-//    savannaUrlRoot: 'http://c2devsav1:8080/c2is2/',
+    // savannaUrlRoot: 'https://c2astsav1.thetuscorp.com/c2is2/',
+   savannaUrlRoot: 'http://c2devsav1:8080/c2is2/',
     //savannaUrlRoot: 'http://10.0.3.169:8080/c2is2/',
 
     helpUrl: 'http://www.google.com',
