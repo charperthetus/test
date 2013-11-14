018a70038ea351f3a123c0a682804df45edd0e8a
plosassothetus <zee4Wai>
2013-11-14 10:25:34 -0800

    Add classification banner and information panel to Map.

--- src/public/app/map/view/MapComponent.js
+++ src/public/app/map/view/MapComponent.js
@@ -1,18 +1,42 @@
 Ext.define('Savanna.map.view.MapComponent', {
-    extend: 'Ext.panel.Panel',
+    extend: 'Savanna.component.ClassificationPanel',
     alias: 'widget.mapcomponent',
 
     requires: [
         'Savanna.map.controller.MapController',
-        'Savanna.map.view.part.OL3MapComponent'
+        'Ext.layout.container.Border',
+        'Savanna.map.view.part.OL3MapComponent',
+        'Ext.layout.container.Fit',
+        'Savanna.metadata.view.part.InformationPanel'
     ],
 
     controller: 'Savanna.map.controller.MapController',
 
     layout: 'border',
 
-    items: [{
-        xtype: 'ol3mapcomponent',
-        itemId: 'ol3Map'
-    }]
+    items: [
+        {
+            xtype: 'ol3mapcomponent',
+            itemId: 'ol3Map',
+            region: 'center'
+        },
+        {
+            xtype: 'panel',
+            item: 'mapSidePanel',
+            region: 'east',
+            width: '30%',
+            layout: 'fit',
+            collapsible: true,
+            header: {
+                ui: 'light-blue'
+            },
+            collapseMode : 'header',
+            headerPosition: 'left',
+            collapsedCls : 'light-blue',
+
+            items: [{
+                xtype: 'informationpanel'
+            }]
+        }
+    ]
 });
